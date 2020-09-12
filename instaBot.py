8da516ca9189d5e2834265cdfb487f20585a0dab
Yeison Stiven Builes Uribe <54378260+ZeuPlox@users.noreply.github.com>
2020-09-12 09:15:49 -0500

    Get instagram post from any profile that you want

--- instaBot.py
+++ instaBot.py
@@ -1,6 +1,4 @@
 import instaloader
-from datetime import datetime
-import time
 import logging
 import os
 import telegram
@@ -16,12 +14,7 @@ L = instaloader.Instaloader(download_comments=False, max_connection_attempts=9, 
 
 logger.info('Connected successfully')
 
-PROFILES = ["checkandplay","elrubiuswtf", "instantgaminges", "levelupcom", "3djuegos"]
-
-date = datetime.now() 
-now = datetime(date.year,date.month,date.day)
-
-timeSleep = 5 #set time to wait between posts
+PROFILES = ["checkandplay", "instantgaminges", "levelupcom", "3djuegos"]
 
 TOKEN = os.getenv("TOKEN") #get bot token
 
@@ -29,51 +22,52 @@ def getStart(update, context):
     bot = context.bot
     chatId = update.message.chat_id
 
-    while True:
-        try:
-            for PROFILE in PROFILES:
-                logger.info(f'Profile = {PROFILE}')
-                print(f'Timeout: {timeSleep}')
-                time.sleep(timeSleep)
-                profile = instaloader.Profile.from_username(L.context, PROFILE) #connect with the profile
-                logger.info('Profile loaded')
+    try:
+        for PROFILE in PROFILES:
+            logger.info(f'Profile = {PROFILE}')                                
+            profile = instaloader.Profile.from_username(L.context, PROFILE) #connect with the profile
+            logger.info('Profile loaded')
+            i = 0
+            for post in profile.get_posts(): #get posts of the profile                          
+                #time.sleep(5)                
+                i+=1
+                download = L.download_post(post,PROFILE)#download post
+                if download==True: #the download has been successfully
+                    video = post.is_video #verify if the post is a video                    
+                    if video==True:                        
+                        try:
+                            with open(post.owner_username+'/'+post.shortcode+'.mp4' , 'rb') as f: #read the file with post
+                                bot.send_video(chat_id=chatId, video=f) #send the video to the chat
+                                if post.caption == None: #if the post has no description
+                                    bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> None')
+                                else: #send message with post description
+                                    bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> {post.caption}')                                
+                        except:
+                            pass   
+                    else: #the post is an image
+                        try:
+                            with open(post.owner_username+'/'+post.shortcode+'.jpg' , 'rb') as f:
+                                bot.send_photo(chat_id=chatId, photo=f) #send image
+                                if post.caption == None:
+                                    bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> None')
+                                else: #send post description
+                                    bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> {post.caption}')                                
+                        except:
+                            pass    
+                if i == 2:
+                    break                
 
-                for post in profile.get_posts(): #get posts of the profile
-                    if post.date >= now: #if the posts were published "today"
-                        print(f'Timeout to download: {timeSleep}')
-                        time.sleep(timeSleep)
-                        download = L.download_post(post,PROFILE)#download post
-                        if download==True: #the download has been successfully
-                            video = post.is_video #verify if the post is a video
-                            if video==True:
-                                try:
-                                    with open(post.owner_username+'/'+post.shortcode+'.mp4' , 'rb') as f: #create and save a file with post
-                                        bot.send_video(chat_id=chatId, video=f) #send the video to the chat
-                                        if post.caption == None: #if the post has no description
-                                            bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> None')
-                                        else: #send message with post description
-                                            bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> {post.caption}')
-                                        
-                                        break
-                                except:
-                                    pass   
-                            else: #the post is an image
-                                try:
-                                    with open(post.owner_username+'/'+post.shortcode+'.jpg' , 'rb') as f:
-                                        bot.send_photo(chat_id=chatId, photo=f) #send image
-                                        if post.caption == None:
-                                            bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> None')
-                                        else: #send post description
-                                            bot.sendMessage(chat_id=chatId, parse_mode='HTML', text=f'<b>{post.owner_username}:</b> {post.caption}')
-                                        
-                                        break
-                                except:
-                                    pass                    
-            
-                logger.info('Next profile')                    
+            logger.info('Next profile')                    
 
-        except:
-            pass
+    except:
+        pass
+        
+
+def pong(update,context):
+    bot = context.bot
+    chatId = update.message.chat_id
+
+    bot.sendMessage(chat_id=chatId, text="pong")
 
 
 
@@ -84,6 +78,7 @@ updater = Updater(myBot.token, use_context=True) #to get info sent to the bot
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start", getStart)) #set command
+dp.add_handler(CommandHandler("ping", pong)) #set command
 
 updater.start_polling()
 print('BOT RUNNING')
